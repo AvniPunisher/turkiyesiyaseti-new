b15d9cc3ef91ac333440b7557ecd831b63fe6300
AvniPunisher <avnipunisher@gmail.com>
2025-04-19 23:37:35 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use((err, req, res, next) => {
 });
 
 // Sunucuyu başlat
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor`);
 });

32817a33c46a4df05c7336f4d3a6c0b37d867408
AvniPunisher <avnipunisher@gmail.com>
2025-04-21 16:27:30 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainMenu from './components/MainMenu';
 import Login from './components/Login';
 import Register from './components/Register';
+import VerifyEmail from './components/VerifyEmail'; // Yeni eklenen bileşen
 import SinglePlayer from './components/SinglePlayer';
 import MultiPlayer from './components/MultiPlayer';
 import LoadGame from './components/LoadGame';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<MainMenu />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/verify-email" element={<VerifyEmail />} /> {/* Yeni eklenen rota */}
         <Route path="/single-player" element={<SinglePlayer />} />
         <Route path="/multi-player" element={<MultiPlayer />} />
         <Route path="/load-game" element={<LoadGame />} />

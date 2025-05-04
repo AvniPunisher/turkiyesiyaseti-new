764d02d3a34c6e3629cb951fe1221933495b3115
AvniPunisher <avnipunisher@gmail.com>
2025-05-04 21:02:24 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import LoadGame from './components/LoadGame';
 import CharacterCreator from './components/CharacterCreator/CharacterCreator';
 import PartyCreator from './components/PartyCreator/PartyCreator';
 import GameDashboard from './components/GameDashboard/GameDashboard';
-import GameScreen from './components/GameScreen/GameScreen';
+import { GameScreen } from './components/GameScreen';
 import GlobalStyle from './GlobalStyle';
 
 // Özel koruma bileşeni - oyun sayfaları için giriş kontrolü
@@ -66,7 +66,7 @@ function App() {
             <GameDashboard />
           </ProtectedRoute>
         } />
-        <Route path="/game-screen" element={
+        <Route path="/game" element={<GameScreen />
           <ProtectedRoute>
             <GameScreen />
           </ProtectedRoute>
